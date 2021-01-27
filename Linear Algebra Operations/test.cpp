5f309f29c19ac9db72ab3c559dd66926f4d6eb8c
noreff <jzyan023@protonmail.com>
2021-01-27 17:04:44 +0100

    update test.cpp

--- Linear Algebra Operations/test.cpp
+++ Linear Algebra Operations/test.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <vector>
-#include <type_traits>
 
 // tested functionalities
 #include "squareMatrix.C"
@@ -8,11 +6,28 @@
 
 int main(int argc, char const *argv[])
 {	
-	std::vector<int> M{11,12,13,21,22,23,31,32,33};
-	squareMatrix<int> myMat(3);
+	// initiaization
+	std::vector<int> M{1,2,3,4,
+					   0,1,0,2,
+					   0,0,1,3,
+					   2,3,1,6};
+	squareMatrix<int> myMat(4);
 	myMat.build(M);
 
-	std::cout << myMat.getSize();
+
+	// test outputs
+	std::cout << "trace of M is " << myMat.trace() << "\n";
+
+	std::cout << "diagonal elements of M are: [";
+	std::vector<int> v = myMat.diag();
+	for (int i = 0; i < v.size()-1; ++i)
+	{
+		std::cout << v[i] << ", ";
+	}
+	std::cout << *(v.end()-1) << "]\n";
+
+	std::cout << "determinant of M is " << myMat.det() << "\n";
+
 
 	return 0;
 }
